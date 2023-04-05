e08d9bb5627f3fd9d0a02cc1889a5202b9d8bb77
SongHae8640 <thdgo456@naver.com>
2023-04-05 09:43:06 +0900

    feat : 개발 환경 구성

---
+++ src/main/java/com/designpattern/apply/DesignPatternApplyApplication.java
@@ -0,0 +1,13 @@
+package com.designpattern.apply;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DesignPatternApplyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DesignPatternApplyApplication.class, args);
+    }
+
+}
